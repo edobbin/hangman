d5e79022f7f477c3db9c9f53b2aca227ae54f81f
edobbin <ethandobbin37@gmail.com>
2024-01-08 08:02:28 -0500

    missing letters

--- hangman.py
+++ hangman.py
@@ -1,2 +1,36 @@
+from user import User
+import random
+
+
 class hangman:
+    def genWordBank(self):
+        f = open('wordlist.txt', 'r')
+        for x in f:
+            self.wordBank.append(x)
     
+            
+    def __init__(self):
+        self.user = User()
+        self.wordBank = []
+        
+        self.genWordBank()
+        self.word = random.choice(self.wordBank)
+    
+    def printWord(self):
+        fin = ""
+        for x in self.word:
+            if x in self.user.letters:
+               fin += x+" "
+            else:
+                fin+= "_ "
+        print(fin)
+                
+    def hangman(self):
+        self.printWord()
+        # while self.user.wrong < 6:
+            
+        
+    
+        
+h = hangman()
+h.hangman()
